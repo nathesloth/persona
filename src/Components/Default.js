6df011153dface5d2b4236238e5101fd14be1db7
Natalie Smith <nataliesmith4122@gmail.com>
2022-10-06 08:03:02 +1000

    BREAKING STUFF

--- src/Components/Default.js
+++ src/Components/Default.js
@@ -3,7 +3,6 @@ import "../index.css";
 import React from "react";
 import { useState } from "react";
 import Axios from "axios";
-import ScriptTag from "react-script-tag";
 
 const Default = (props) => {
     const [responseList, setResponseList] = useState([]);
