62c87434eb3776a2619137811cb0fafcc1d70676
Natalie Smith <nataliesmith4122@gmail.com>
2022-10-11 16:11:09 +1000

    test

--- src/Components/Test.js
+++ src/Components/Test.js
@@ -2,12 +2,10 @@ import "../App.css";
 import "../index.css";
 import "../home.css";
 import React, { useState, useEffect } from "react";
-import Rellax from 'rellax';
 import { Link } from "react-router-dom";
 
 
 const Test = (props) => {
-
     return (
         <div>
             <div class="containerA " data-rellax-speed="8" id="containerA">
